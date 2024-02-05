5d5d5119bead314e4d8e4a0a7db62e3edd73955d
avtew <avtew@outlook.com>
2024-02-05 17:49:06 +0100

    fix: add 'key' prop to links object

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -3,14 +3,15 @@ import Link from 'next/link'
 // import Avatar from './Avatar';
 
 type LinkPropsType = {
+  key: string
   title: string
   href: string
 }
 
 const links = [
-  { title: 'Tournaments', href: '/cabinet/tournaments', },
-  { title: 'Games', href: '/cabinet/games' },
-  { title: 'Players', href: '/cabinet/players' },
+  { id: '1', title: 'Tournaments', href: '/cabinet/tournaments', },
+  { id: '2', title: 'Games', href: '/cabinet/games' },
+  { id: '3', title: 'Players', href: '/cabinet/players' },
 ];
 
 function NavLink(props: LinkPropsType) {
@@ -20,7 +21,7 @@ function NavLink(props: LinkPropsType) {
 export default function Aside() {
   return (
     <nav className='flex flex-col gap-2 p-4 pr-5 border-r'>
-      {links.map(link => <NavLink title={link.title} href={link.href} />)}
+      {links.map(link => <NavLink key={link.id} title={link.title} href={link.href} />)}
     </nav>
   )
 }
